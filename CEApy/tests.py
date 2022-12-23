2c663e9bb53711ef479be7ff3e367c9e1cf34f7c
juliomachad0 <julioromac@outlook.com>
2022-12-23 17:20:26 -0300

    Atualizacao e organizacao dos metodos 23 do 12 de 2022

--- CEApy/tests.py
+++ CEApy/tests.py
@@ -1,133 +1,39 @@
+import logging as log
 from CEApy import CEA
-from time import sleep
-import pandas as pd
 import matplotlib.pyplot as plt
 
-import pandas as pd
-import numpy as np
-
-def data_exit_condition(df,cond):
-    df_new = None
-    c = 0
-    for cln in df.columns:
-        cl = []
-        for j in range(cond, len(df[cln]), 3):
-            cl.append(df[cln][j])
-        if c == 0:
-            df_new = pd.DataFrame(cl, columns=[cln])
-        elif c != 0:
-            df_new[cln] = cl
-        c = c + 1
-    return df_new
+oxidante = [['N2O', 100, 298.15]]
+fuel = [['CH3NO2(L)', 100, 298.15]]
 
 
-def plot_curve(x, y, leg_x, leg_y, tit, curva):
-    plt.plot(x, y, 'o--', label=curva)
-    plt.legend()
-    plt.xlabel(leg_x)
-    plt.ylabel(leg_y)
-    plt.title(leg_x + ' X ' + leg_y+tit)
+def if_string_to_lower(column_names):
+    if type(column_names) is list:
+        for i in column_names:
+            if type(i) is str:
+                pass
+            else:
+                log.error('CEA: {} item is not a string\n'.format(i))
+                return
+        column_names = [s.replace(s, s.lower()) for s in column_names]
+    return column_names
 
 
-# INICIANDO ANALISE
-oxidante = [['N2O', 100, 300]]  # OXIDO NITROSO
-aeat = [100, 150, 170, 200, 220, 250]
-of = [0.5, 0.75, 1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5]
-pressure = [10]  # bar
-# L22 Nitrometano
-L22_nitro = CEA('L22_analise_nitrometano')
-fuel_nitro = [['CH3NO2(L)', 100, 298.15]]
-L22_nitro.input_oxids(oxidante)
-L22_nitro.input_fuels(fuel_nitro)
-L22_nitro.input_of_ratio(of)
-L22_nitro.input_pressure_chamber(pressure)
-L22_nitro.input_frozen_station('sim', 'exit')
-L22_nitro.enable_short_output()
-L22_nitro.enable_transport_prop()
-L22_nitro.output_isp()
-L22_nitro.output_thrust_coef()
-L22_nitro.output_of_ratio()
-L22_nitro.output_aeat()
+aeat = [200]
+pressure = [10]
+of = [0.5, 0.75, 1, 2, 3, 4]
+L22 = CEA()
+L22.settings()
+L22.input_propellants(oxid=oxidante, fuel=fuel)
+L22.input_parameters(chamber_pressure=pressure, sup_aeat=aeat, of_ratio=of)
+L22.output_parameters(['cf', 'isp', 'gam', 'pipe', 'o/f', 'ivac'])
+L22.run()
+df = L22.get_results('all', 'exit')
 
-L22_nitro.input_sup_aeat([200])
-L22_nitro.run()
-aeat_isp = L22_nitro.get_results(['cf', 'o/f'], 'throat')
+if df is not None:
+    df['isp'] = df['isp']/9.8
+    df['ivac'] = df['ivac']/9.8
+    print(df.head())
 
-# print('curva aeat x isp: \n', aeat_isp)
-
-
-"""
-# ANALISE NITROMETANO
-df_nitro_100 = None
-df_nitro_150 = None
-df_nitro_170 = None
-df_nitro_200 = None
-df_nitro_220 = None
-df_nitro_250 = None
-cont = 0
-for i in aeat:
-    print('teste {}'.format(cont))
-    L22_nitro.input_sup_aeat([i])
-    L22_nitro.run()
-    sleep(1)
-    df_nitro = L22_nitro.get_results()
-    if cont == 0:
-        df_nitro_100 = data_exit_condition(df_nitro)
-        print(df_nitro_100["isp"][0])
-    if cont == 1:
-        df_nitro_150 = data_exit_condition(df_nitro)
-    if cont == 2:
-        df_nitro_170 = data_exit_condition(df_nitro)
-    if cont == 3:
-        df_nitro_200 = data_exit_condition(df_nitro)
-    if cont == 4:
-        df_nitro_220 = data_exit_condition(df_nitro)
-    if cont == 5:
-        df_nitro_250 = data_exit_condition(df_nitro)
-    print('iter: {}, value: {}\n'.format(cont, i))
-    cont = cont + 1
-"""
-"""
-# ISP
-df_nitro_100['isp'] = df_nitro_100['isp'] / 9.81
-plot_curve(df_nitro_100['o/f'], df_nitro_100['isp'],
-           'O/F', 'Isp (s)',
-           ' Nitrometano',
-           'aeat=100')
-
-df_nitro_150['isp'] = df_nitro_150['isp'] / 9.81
-plot_curve(df_nitro_150['o/f'], df_nitro_150['isp'],
-           'O/F', 'Isp (s)',
-           ' Nitrometano',
-           'aeat=150')
-
-df_nitro_170['isp'] = df_nitro_170['isp'] / 9.81
-plot_curve(df_nitro_170['o/f'], df_nitro_170['isp'],
-           'O/F', 'Isp (s)',
-           ' Nitrometano',
-           'aeat=170')
-
-df_nitro_200['isp'] = df_nitro_200['isp'] / 9.81
-plot_curve(df_nitro_200['o/f'], df_nitro_200['isp'],
-           'O/F', 'Isp (s)',
-           ' Nitrometano',
-           'aeat=200')
-
-df_nitro_220['isp'] = df_nitro_220['isp'] / 9.81
-plot_curve(df_nitro_220['o/f'], df_nitro_220['isp'],
-           'O/F', 'Isp (s)',
-           ' Nitrometano',
-           'aeat=220')
-
-df_nitro_250['isp'] = df_nitro_250['isp'] / 9.81
-plot_curve(df_nitro_250['o/f'], df_nitro_250['isp'],
-           'O/F', 'Isp (s)',
-           ' Nitrometano',
-           'aeat=250')
+plt.plot(df['gam'], df['isp'])
+print(df['gam'])
 plt.show()
-"""
-# L22_nitro.vizualize_output_file()
-"""
-CEA().search_specie('N2O')
-CEA().show_all_species()
-"""
